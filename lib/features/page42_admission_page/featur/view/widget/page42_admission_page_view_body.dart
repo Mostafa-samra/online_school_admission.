12571fbd50c8cf3efd427808a755d212b6c308d7
Mostafa-samra <moonflay300@gmail.com>
2024-06-15 10:13:37 +0300

    page42

--- lib/features/page42_admission_page/featur/view/widget/page42_admission_page_view_body.dart
+++ lib/features/page42_admission_page/featur/view/widget/page42_admission_page_view_body.dart
@@ -11,7 +11,7 @@ class Page42AdmissionPageViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView(children: [
+    return ListView(children: const [
 
        CustomBackIconButton(),
         TitleTextAndDivider(),
